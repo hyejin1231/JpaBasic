d79b901b3706df108ae69948082fd264336d64bc
hyejin1231 <phj981231@naver.com>
2022-03-15 22:23:10 +0900

    20220315 JPQL 소개

--- src/main/java/jpa/basic/hello2/helloJpa.java
+++ src/main/java/jpa/basic/hello2/helloJpa.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class helloJpa {
     public static void main(String[] args) {
@@ -18,6 +19,11 @@ public class helloJpa {
             member.setName("userA");
 
             em.persist(member);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member member1 : result) {
+                System.out.println("member name = " + member1.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
